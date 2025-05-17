31d0ef5618312388dee9462f172a2263bd17eca1
Sergio <198915914+LastSerbelou@users.noreply.github.com>
2025-05-17 13:42:48 +0200

    crear perfil

--- proyecto_recetas/app/Http/Controllers/Auth/RegisteredUserController.php
+++ proyecto_recetas/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Perfil;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -40,6 +41,14 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
             'user_type' => 0,
+        ]);
+
+        Perfil::create([
+            'id_user' => $user->id,
+            'name' => "perfilPorDefecto",
+            'img_perfil' => "manolo",
+            'img_banner' => "nulo",
+            'biografia' => "Por rellenar"
         ]);
 
         event(new Registered($user));
